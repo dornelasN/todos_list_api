552b9d9d7613187e49b03af3e0e7fc717433a27e
Nelson Dornelas <ndornelasj@gmail.com>
2018-01-26 13:48:22 -0800

    Todos controller methods and helpers

--- spec/requests/todos_spec.rb
+++ spec/requests/todos_spec.rb
@@ -37,7 +37,7 @@ RSpec.describe 'Todos API', type: :request do
     end
 
     context 'when the record does not exist' do
-      let(:todo_item) { 100 }
+      let(:todo_id) { 100 }
 
       it 'returns status code 404 - Not Found' do
         expect(response).to have_http_status(404)
